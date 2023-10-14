9ef25221b6829df9b08208224fcf53321e2ba6f2
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-14 22:14:08 +0200

    page 'new' setting + code that only runs on server side

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link"
+
+async function createTodo(data: FormData) {
+    // it will never run on the client (need to add experimental.serverActions)
+    "use server"
+
+    console.log("Hi")
+}
+
 export default function Page() {
-    return <h1>New page</h1>
+    return <>
+        <header className="flex justify-between item-center mb-4">
+            <h1 className="text-2xl">New</h1>
+        </header>
+        <form action={createTodo} className="flex gap-2 flex-col">
+            <input type="text" name="title" className="border border-slate-300 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100" />
+            <div className="flex gap-1 justify-end">
+                <Link href=".." className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Cancel</Link>
+                <button type="submit" className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">Create</button>
+            </div>
+        </form>
+    </>
 }
