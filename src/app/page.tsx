e7af2cc94857ce1c648b3675935a4f7cf9f52ad7
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-14 21:45:54 +0200

    create manualy one todos - must have 'await' before

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default async function Home() {
 
   const todos = await prisma.todo.findMany()
-  prisma.todo.create({ data: { title: "test", complete: false } })
+  // await prisma.todo.create({ data: { title: "test", complete: false } })
   return (
     <>
       <header className="flex justify-between item-center mb-4">
