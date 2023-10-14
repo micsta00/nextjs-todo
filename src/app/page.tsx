e2d44e681f4e55e7c21376099afe913a5a1a7647
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-14 21:42:32 +0200

    added inline styles + map todos + create manualy one todo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <h1>Hi</h1>
+import { prisma } from "@/db"
+import Link from "next/link"
+
+prisma.todo.create({ data: { title: "test", complete: false } })
+
+export default async function Home() {
+
+  const todos = await prisma.todo.findMany()
+  return (
+    <>
+      <header className="flex justify-between item-center mb-4">
+        <h1 className="text-2xl">Todos</h1>
+        <Link className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" href="/new">New</Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
