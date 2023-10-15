6f4f995ef5776252bba0daacf67fcef9782fd04b
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-15 21:55:25 +0200

    added delete option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from "@/db"
 import Link from "next/link"
 import { TodoItem } from "@/components/TodoItem"
+import { redirect } from "next/navigation"
+import { revalidatePath } from "next/cache"
 
 function getTodos() {
   return prisma.todo.findMany()
@@ -10,6 +12,13 @@ async function toggleTodo(id: string, complete: boolean) {
   "use server"
 
   await prisma.todo.update({ where: { id }, data: { complete } })
+}
+
+async function deleteTodo(id: string) {
+  "use server"
+
+  await prisma.todo.delete({ where: { id } })
+  revalidatePath("/")
 }
 
 export default async function Home() {
@@ -22,7 +31,7 @@ export default async function Home() {
       </header>
       <ul className="pl-4">
         {todos.map(todo => (
-          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} />
+          <TodoItem key={todo.id} {...todo} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
         ))}
       </ul>
     </>
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -5,16 +5,19 @@ type TodoItemProps = {
     title: string
     complete: boolean
     toggleTodo: (id: string, complete: boolean) => void
+    deleteTodo: (id: string) => void
 }
 
-export function TodoItem({ id, title, complete, toggleTodo }: TodoItemProps) {
-    return <li className="flex gap-1 items-center">
+export function TodoItem({ id, title, complete, toggleTodo, deleteTodo }: TodoItemProps) {
+    return <li className="flex gap-2 items-center">
         <input
             id={id}
             type="checkbox"
             className="cursor-pointer peer"
             defaultChecked={complete}
-            onChange={e => toggleTodo(id, e.target.checked)} />
+            onChange={e => toggleTodo(id, e.target.checked)}
+        />
         <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-500">{title}</label>
+        <button type="button" className="border border-slate-300 text-slate-300 px-2 py-1 m-2 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" onClick={e => deleteTodo(id)}>Delete</button>
     </li>
 }
